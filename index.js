e7fbefb22d7603b9270b8590799130e8a12b8a59
German Koning <german@geko.com.ar>
2022-04-01 14:31:50 -0300

    commit

--- index.js
+++ index.js
@@ -10,31 +10,42 @@ if (vart == clave) {
 
 
 
-
-while (vart == clave) {
-    let id = 0
+while (vart == clave ) {
+    let id = localStorage.length++
     console.log("iniciando")
     let var1 = prompt("Ingrese su nombre")
     let var2 = prompt("Ingrese su apellido")
     let var3 = prompt("ingrese su email")
     let var4 = prompt("ingrese su direccion")
-
+  
     var persona = {
         nombre: var1,
         apellido: var2,
         email: var3,
         direccion: var4
     };
-
+    
     localStorage.setItem(id, JSON.stringify(persona));
     id = id++
+        
+    
+    
+    
+    alert("Usuario generado con los siguientes datos " + JSON.stringify(persona))
+       
+        
+    break
+    
     
 
+}
 
-
-    alert("Usuario generado con los siguientes datos " + JSON.stringify(persona))
-   
-    break
+for (var i = 0; i < localStorage.length; i++){
+    var temp = localStorage.getItem(i);
+    const para = document.createElement("p");
+    para.innerText = temp;
+    document.body.appendChild(para);
 
 
 }
+
