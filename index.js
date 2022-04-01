e859349256a0bd3229fffcad9a18a86e6b8973e5
German Koning <german@geko.com.ar>
2022-04-01 12:44:11 -0300

    data

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+while (true) {
+    let var1 = prompt("ingrese su nombre")
+    let var2 = prompt("Ingrese su apellido")
+    let var3 = prompt("ingrese su email")
+    let var4 = prompt("ingrese su direccion")
+
+    var persona = {
+        nombre: var1,
+        apellido: var2,
+        email: var3,
+        direccion: var4
+    };
+
+    
+
+}
