c02798ac06700067108a55375cff662e5afbb941
German Koning <german@geko.com.ar>
2022-04-01 14:04:44 -0300

    commit

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
+/* German Koning - Comision 31640 */ 
+let puntero = 0
+let vart = prompt("ingrese su token")
+let clave = "123"
+if (vart == clave) {
+    alert("Bienvenido")
+} else {
+    alert("Acceso denegado")
+}
 
 
-while (true) {
-    let var1 = prompt("ingrese su nombre")
+
+
+while (vart == clave) {
+    let id = 0
+    console.log("iniciando")
+    let var1 = prompt("Ingrese su nombre")
     let var2 = prompt("Ingrese su apellido")
     let var3 = prompt("ingrese su email")
     let var4 = prompt("ingrese su direccion")
@@ -13,6 +26,15 @@ while (true) {
         direccion: var4
     };
 
+    localStorage.setItem(id, JSON.stringify(persona));
+    id = id++
     
+
+
+
+    alert("Usuario generado con los siguientes datos " + JSON.stringify(persona))
+   
+    break
+
 
 }
